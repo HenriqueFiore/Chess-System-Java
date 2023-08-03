8246b51957fdbf4ace6a9bc17c68d996de4c0672
Henrique Silveira Fiorentin <86538681+HenriqueFiore@users.noreply.github.com>
2023-08-03 18:54:07 -0300

    movement rules for rook created

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -46,6 +47,15 @@ public class ChessMatch {
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible move fpr the chosen piece");
 		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
+			
+		}
+		
+		
 	}
 
 	private void placeNewPiece(int column, int row, ChessPiece piece) {
